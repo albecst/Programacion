65967bd02849fc87572b9c9df17a5b4a054d0511
Alberto <148889956+AlbeTroll@users.noreply.github.com>
2024-03-01 00:00:18 +0100

    Create Número de cifras.java

---
+++ Sesion 3/Número de cifras.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    int n = entrada.nextInt();
+    int longitud;
+    String n_str = n+"";
+    longitud = n_str.length();
+
+    System.out.println(longitud);
+      
+  }
+}
